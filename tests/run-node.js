62a640b05405ee849416141a1784d618450087f4
kot0070 <95998304+kot0070@users.noreply.github.com>
2026-09-15 15:53:56 -0500

    Run document compliance regression tests

--- tests/run-node.js
+++ tests/run-node.js
@@ -22,6 +22,7 @@ require('../electric-phase3.js');
 require('./electrical-calculators.test.js');
 require('./data-integrity.test.js');
 require('./navigation-shell.test.js');
+require('./document-compliance.test.js');
 require('./residential-estimator.test.js');
 require('./residential-pricing.test.js');
 require('./residential-takeoff.test.js');

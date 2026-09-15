4574100cefcbf8bc3ea520595d60f4520bd7446e
kot0070 <95998304+kot0070@users.noreply.github.com>
2026-09-15 14:34:38 -0500

    Load navigation bridge after workspace shell

--- sw-register.js
+++ sw-register.js
@@ -1,6 +1,17 @@
 (function () {
   'use strict';
 
+  function loadNavigationBridge() {
+    if (/electrical-tools\.html$/i.test(location.pathname)) return;
+    if (document.querySelector('script[data-be-nav-bridge]')) return;
+    var b = document.createElement('script');
+    b.src = './electric-navigation-bridge.js';
+    b.defer = true;
+    b.dataset.beNavBridge = '1';
+    b.onerror = function () {};
+    document.head.appendChild(b);
+  }
+
   function loadWorkspaceEnhancement() {
     // Electrical Tools owns its own shell; only enhance the legacy estimating workspace.
     if (/electrical-tools\.html$/i.test(location.pathname)) return;
@@ -9,6 +20,7 @@
     s.src = './electric-workspace.js';
     s.defer = true;
     s.dataset.beWorkspace = '1';
+    s.onload = loadNavigationBridge;
     s.onerror = function () { /* fail-open: legacy navigation remains usable */ };
     document.head.appendChild(s);
   }

63266d227739c56421bbdc7da3b0943b2b1150e3
kot0070 <95998304+kot0070@users.noreply.github.com>
2026-09-15 06:48:52 -0500

    Correct NEC 2026 dwelling load and residential rule metadata

--- electric-residential-rules.js
+++ electric-residential-rules.js
@@ -4,7 +4,7 @@
 (function(root){
 'use strict';
 const R=Object.freeze({
-  version:'residential-phase2-2026',
+  version:'residential-phase2-2026-r2',
   references:Object.freeze({
     minimumUnitLoad:'NEC 2026 120.41',
     lightingDemand:'NEC 2026 Table 120.45',
@@ -12,11 +12,13 @@ const R=Object.freeze({
     fixedAppliances:'NEC 2026 120.53',
     dryer:'NEC 2026 120.54',
     cooking:'NEC 2026 120.55',
-    noncoincident:'NEC 2026 120.60',
+    noncoincident:'NEC 2026 120.6 / applicable equipment-specific load rules',
+    motors:'NEC 2026 120.50; 430.24',
     serviceConductors:'NEC 2026 310.12'
   }),
   minimum:Object.freeze({
-    generalVAperFt2:3,
+    generalVAperFt2:2,
+    branchCircuitGeneralVAperFt2:3,
     smallApplianceCircuits:2,
     smallApplianceVAperCircuit:1500,
     laundryCircuits:1,
@@ -24,7 +26,7 @@ const R=Object.freeze({
     bathroom20ACircuits:1
   }),
   lightingDemand:Object.freeze({firstVA:3000,firstPct:1,midLimitVA:120000,midPct:.35,overPct:.25}),
-  fixedApplianceDemand:Object.freeze({minimumCount:4,factor:.75}),
+  fixedApplianceDemand:Object.freeze({minimumCount:4,factor:.75,minimumWatts:500,minimumHp:.25,excludedCategories:Object.freeze(['cooking','dryer','space-heating','air-conditioning','evse'])}),
   dryer:Object.freeze({fallbackVA:5000}),
   singleCooking:Object.freeze({maxAutoNameplateKW:12,columnCDemandVA:8000}),
   serviceRatings:Object.freeze([100,125,150,175,200,225,250,300,350,400]),

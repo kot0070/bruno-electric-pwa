f8e1d4d4339bacbd4c6e6f07a17427fdc056d2c9
kot0070 <95998304+kot0070@users.noreply.github.com>
2026-09-16 06:51:01 -0500

    Run Catalog and Job Materials semantic regression suite

--- tests/run-node.js
+++ tests/run-node.js
@@ -40,6 +40,7 @@ require('./residential-wire-takeoff.test.js');
 require('./residential-save-archive-ux.test.js');
 require('./job-summary-semantics.test.js');
 require('./quote-lifecycle.test.js');
+require('./catalog-job-ux-semantics.test.js');
 require('./dispatch-journal-v2.test.js');
 require('./project-calculator.test.js');
 require('./phase3-equipment.test.js');

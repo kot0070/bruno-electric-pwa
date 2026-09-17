fc3c57e31244446c33eb6e56929e6bfeec1c28c6
kot0070 <95998304+kot0070@users.noreply.github.com>
2026-09-17 10:42:16 -0500

    Run Stage 4 calculator boundary tests

--- tests/run-node.js
+++ tests/run-node.js
@@ -31,6 +31,7 @@ require('../electric-evse-professional.js');
 require('../electric-custom-materials.js');
 require('../electric-pricing-domain-guard.js');
 require('./electrical-calculators.test.js');
+require('./stage4-calculator-boundary.test.js');
 require('./electric-bom.test.js');
 require('./math-corrective-boundaries.test.js');
 require('./electrical-tasks.test.js');
